debf47698f342a2c23e6f7ae4fd2ef297e27bef1
Ashleigh Newman-Jones <ashnewman-jones@hotmail.co.uk>
2026-02-12 22:23:00 +0000

    Require env

--- src/env.ts
+++ src/env.ts
@@ -6,6 +6,7 @@ export const env = createEnv({
     CONVEX_DEPLOYMENT: z.string().min(1),
     RESEND_KEY: z.string().min(1),
     STRIPE_SECRET_KEY: z.string().min(1),
+    CLERK_SECRET_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_CONVEX_URL: z.url(),
@@ -13,6 +14,7 @@ export const env = createEnv({
     NEXT_PUBLIC_POSTHOG_KEY: z.string().min(1),
     NEXT_PUBLIC_POSTHOG_HOST: z.string().min(1),
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
@@ -21,5 +23,7 @@ export const env = createEnv({
     NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   },
 });
